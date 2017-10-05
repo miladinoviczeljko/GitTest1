f19a44c378d2057677759241c1475faea1662c1e
Zeljko Miladinovic <z.miladinovic@levi9.com>
2017-10-05 13:56:40 +0200

    Added Feature 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,9 @@ namespace ConsoleApp1
             // Code was edited in GitHub
             // Code was added in VS
 
-            // Code to call Feature1
+            // Code to call Feature 1
+
+            // Code to call Featrure 2
         }
     }
 }
