e8b91ff109d8690223e8485fd9987054f51a1746
Zeljko Miladinovic <z.miladinovic@levi9.com>
2017-10-05 15:55:34 +0200

    init

---
+++ SimpleWebApi/SimpleWebApi/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebApi.Controllers
+{
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
