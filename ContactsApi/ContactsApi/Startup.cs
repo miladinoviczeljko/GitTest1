e7098632f83cdb9bd2c50125582a3be7d84f29e9
Zeljko Miladinovic <z.miladinovic@levi9.com>
2017-10-06 12:54:21 +0200

    Initial

---
+++ ContactsApi/ContactsApi/Startup.cs
@@ -0,0 +1,83 @@
+﻿using ContactsApi.Contexts;
+
+using ContactsApi.Repository;
+
+using Microsoft.AspNetCore.Builder;
+
+using Microsoft.AspNetCore.Hosting;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json.Serialization;
+
+
+
+namespace ContactsApi
+
+{
+
+    public class Startup
+
+    {
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+
+        {
+
+            Configuration = configuration;
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+
+            // Add framework services.
+
+            services.AddMvc()
+
+                    .AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+
+
+            services.AddDbContext<ContactsContext>(options =>
+
+                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
+
+            //using Dependency Injection
+
+            services.AddSingleton<IContactsRepository, ContactsRepository>();
+
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+
+        {
+
+            //app.ApplyUserKeyValidation();
+
+            app.UseMvc();
+
+        }
+
+    }
+
+}
